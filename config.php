f7205f0ae751169e9065135aa248248b613162b2
Tono2406 <marcos957_0@hotmail.com>
2024-07-29 22:44:13 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "marcos_loor";
+$servername = "bi0aqi7zz0lebqqotyb2-mysql.services.clever-cloud.com";
+$username = "uibqp4selzd2lnsl";
+$password = "JXheMSoK2121Z87qfJtI";
+$dbname = "bi0aqi7zz0lebqqotyb2";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
